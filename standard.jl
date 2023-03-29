fd8c8f49a7999f88a11236d7df285f4ed7689cf9
MiguelGCoelho <72024990+MiguelGCoelho@users.noreply.github.com>
2023-03-29 22:17:39 +0100

    CIP - Initialize
    Takes into account the initform if exists

--- standard.jl
+++ standard.jl
@@ -18,18 +18,38 @@ function allocate_instance(class)
   PavaObj(obj)
 end
 
-function initialize(instance, initargs)
-  if length(initargs) == length(instance.class_of.slots)
-    i = 1
-    for (k, v) in initargs
-      if k == instance.class_of.slots[i]
-        instance.slots[k] = v
-      else
-        break
+function check_initform(args)
+  for i in args
+    if isa(i, Expr)
+      if in(:initform, i.args)
+        return i.args[2]
       end
-    i += 1
     end
   end
+  return missing
+end
+
+function initialize(instance, initargs)
+  println(instance.class_of.slots)
+  Args = []
+  for i in (instance.class_of.slots)
+    j = (i.args)
+    println(j)
+    k = j[1]
+    push!(Args, k)
+    value = check_initform(j)
+    instance.slots[k] = value
+  end
+  println(Args)
+  for (k, v) in initargs
+    if k in Args 
+      println(k, v)
+      instance.slots[k] = v
+    else
+      println("Argument: ", k, " isn't defined in the class: ", instance.class_of.name)
+    end
+  end
+  println(instance.slots)
 end
 
 function new(class; initargs...)
@@ -38,7 +58,11 @@ function new(class; initargs...)
     instance
   end
 end
-@defclass(ComplexNumber, [], [])
-c1 = new(ComplexNumber)
-println(c1)
+
+@defclass(Person, [],
+[[name, reader=get_name, writer=set_name!],
+[age, reader=get_age, writer=set_age!, initform=0],
+[friend, reader=get_friend, writer=set_friend!]],
+metaclass=UndoableClass)
+c1 = new(Person)
 end
